542cbcb303e390eb424d0c295fbf5af87cbcc716
suzp1984 <suzp1984@gmail.com>
2016-07-11 21:40:32 +0800

    use jdee-2.4.1

--- config/my-jdee.el
+++ config/my-jdee.el
@@ -1,8 +1,8 @@
 ;;
 (setq debug-on-error t)
 
-;; (add-to-list 'load-path (expand-file-name "~/.emacs.d/emacs-lisp/jdee-2.4.1/lisp/"))
-;; (autoload 'jde-mode "jde" "JDE mode" t)
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/emacs-lisp/jdee-2.4.1/lisp/"))
+(autoload 'jde-mode "jde" "JDE mode" t)
 (setq auto-mode-alist
       (append '(("\\.java\\'" . jde-mode)) auto-mode-alist))
 
